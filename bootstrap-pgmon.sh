ecbce17a8e80a68d539fd6c02a89e7fc46b35303
Melville Ezeogu <Melville.Ezeogu@cfpb.gov>
2016-01-14 15:12:50 -0500

    removed yum lines

--- bootstrap-pgmon.sh
+++ bootstrap-pgmon.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-sudo yum -y groupinstall 'Development Tools'
-sudo yum -y install java-1.7.0*
-sudo yum -y install yum localinstall http://yum.postgresql.org/9.4/redhat/rhel-6-x86_64/pgdg-centos94-9.4-1.noarch.rpm
-sudo yum -y install postgresql9*
-sudo yum -y install tree
-sudo yum -y install vim
 
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
